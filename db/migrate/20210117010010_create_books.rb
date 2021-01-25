2b7dc9edb84688af209d3c8ba010ccdd1015a5ae
milan2229 <milan_1899.22@icloud.com>
2021-01-25 05:54:56 +0000

    anew commit

--- db/migrate/20210117010010_create_books.rb
+++ db/migrate/20210117010010_create_books.rb
@@ -2,8 +2,8 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.text :body
-
+      t.text :opinion
+      
       t.timestamps
     end
   end
